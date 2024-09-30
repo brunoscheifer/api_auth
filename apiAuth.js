bbb65e7e2d1dbb1a509ba41251dea5b3768fc0a4
brunoscheifer <brunoscheifer11@gmail.com>
2024-09-30 16:21:06 -0300

    .

---
+++ apiAuth.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+const jwt = require('jsonwebtoken')
+const SECRET = 'teste'
+
+function verifyJWT(req, res, next) {
+    const token = req.headers['x-access-token']
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if(err) { 
+            return res.status(401).end()
+        }
+        req.userId = decoded.userId
+        next()
+    })
+}
+
+app.post('/login', (req, res) => {
+    if(req.body.user === 'eu' && req.body.senha === 'senha') {
+        const token = jwt.sign({userId: 1}, SECRET, { expiresIn: 300 })
+        return res.json({ auth: true, token})
+    }
+
+    res.status(401).end()
+})
+
+
+
+app.get('/clima/:cidade', verifyJWT, async (req, res) => {
+console.log(req.userId + ' fez essa chamada') 
+const { cidade } = req.params
+    try {
+        const resultado = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid={API_KEY}&units=metric`)
+        res.status(200).send(resultado.data)
+    } catch (error) {
+        res.status(500).send(console.error(error));
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em https://localhost:3000')
+})
