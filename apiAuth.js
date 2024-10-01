85e015ecc1dd4166700b023a14db9a3b50bc78df
brunoscheifer <brunoscheifer11@gmail.com>
2024-10-01 16:17:48 -0300

    signup

--- apiAuth.js
+++ apiAuth.js
@@ -2,13 +2,31 @@ const axios = require('axios')
 const express = require("express")
 const app = express()
 app.use(express.json())
+const bcrypt = require("bcrypt")
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const jwt = require('jsonwebtoken')
-const SECRET = 'teste'
+
+mongoose
+  .connect('mongodb://localhost:27017/usuarios')
+  .then(() => {
+    console.log("Conectado");
+  })
+  .catch((erro) => {
+    console.error("Erro ao conectar", erro);
+  });
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    senha: { type: String, required: true}
+})
+
+const User = mongoose.model('User', userSchema)
 
 function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token']
-    jwt.verify(token, SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if(err) { 
             return res.status(401).end()
         }
@@ -17,13 +35,38 @@ function verifyJWT(req, res, next) {
     })
 }
 
-app.post('/login', (req, res) => {
-    if(req.body.user === 'eu' && req.body.senha === 'senha') {
-        const token = jwt.sign({userId: 1}, SECRET, { expiresIn: 300 })
-        return res.json({ auth: true, token})
+app.post('/signup', async (req, res) => {
+    const {username, senha} = req.body
+
+    const jaexiste = await User.findOne({ username })
+    if(jaexiste) {
+        return res.status(400).json({ mensagem: 'Usuario ja existe!!'})
     }
 
-    res.status(401).end()
+    const senhahash = await bcrypt.hash(senha, 10)
+    const user = new User({ username, senha: senhahash})
+    try {
+        await user.save();
+        return res.status(201).json({ mensagem: 'Usuário criado com sucesso!' });
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro ao criar usuário', error });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, senha } = req.body
+
+    const user = await User.findOne({ username })
+    if (!user) {
+        return res.status(400).json({ mensagem: 'Usuario ou senha invalidos!'})
+    }
+
+    const certo = await bcrypt.compare(senha, user.senha)
+    if (!certo) {
+        return res.status(400).json({ message: 'Usuário ou senha inválidos.' });
+    }
+        const token = jwt.sign({userId: 1}, process.env.JWT_SECRET, { expiresIn: 300 })
+        return res.json({ auth: true, token})
 })
 
 
