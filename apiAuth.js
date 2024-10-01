82eaf8314988661c83faf27e3b4f08fc21c86551
brunoscheifer <brunoscheifer11@gmail.com>
2024-10-01 13:51:53 -0300

    ignore

--- apiAuth.js
+++ apiAuth.js
@@ -32,7 +32,7 @@ app.get('/clima/:cidade', verifyJWT, async (req, res) => {
 console.log(req.userId + ' fez essa chamada') 
 const { cidade } = req.params
     try {
-        const resultado = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=b13898ce929b46db5d48384497369308&units=metric`)
+        const resultado = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid={API_KEY}&units=metric`)
         res.status(200).send(resultado.data)
     } catch (error) {
         res.status(500).send(console.error(error));
